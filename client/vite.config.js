2b5ed754524f23c37f86e8baf759ac25009c1e7c
Klevis Hënkoja <113030599+KlevisHenkoja@users.noreply.github.com>
2024-09-28 14:52:05 +0200

    sign in/up page and functionallity

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // vendosni portin e duhur
+        secure:false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
