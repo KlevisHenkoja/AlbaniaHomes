8cced20e5a3e58641d80d5168b78bf01e1f6feef
Klevis Hënkoja <113030599+KlevisHenkoja@users.noreply.github.com>
2024-10-29 17:40:00 +0100

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
     const sendEmail = (e) => {
       e.preventDefault();
   
-      emailjs.sendForm('service_z4z48m4', 'template_hmji9he', form.current, 'v9UM2vFmhNbgJlPLH');
+      emailjs.sendForm('service_8oi8zmr', 'template_hmji9he', form.current, 'v9UM2vFmhNbgJlPLH');
   
       e.target.reset();
     };
@@ -191,21 +191,7 @@ export default function Home() {
             <h1 className='text-white text-3xl font-bold text-center mx-auto'>Contact Us</h1>
         </div>
         <div className=" flex justify-around my-5 items-start ">
-            <div className="flex flex-col gap-5 my-3 ">
-                <article className='bg-blue-950 p-5 rounded-xl text-center border-2 border-blue-950 mx-auto transition hover:bg-transparent hover:border-primary-variant'>
-                    <HiOutlineMail className='text-[1.5rem] mb-2 mx-auto text-white'/>
-                    <h4 className='font-semibold text-white'>Email</h4>
-                    <h5 className='text-white'>info@albaniahomes.uk</h5>
-                    <a href="mailto:info@albaniahomes.uk" target="_blank" className='mt-3 inline-block text-[0.8rem] text-blue-400 font-semibold'>Send Email</a>
-                </article>
-                
-                <article className='bg-blue-950 px-9 py-5 rounded-xl text-center border-2 border-blue-950 transition hover:bg-transparent hover:border-primary-variant mx-auto'>
-                    <ImWhatsapp className='text-[1.5rem] mb-2 mx-auto text-white'/>
-                    <h4 className='text-white font-semibold'>WhatsApp</h4>
-                    <h5 className='text-white'>+447786921525</h5>
-                    <a href="https://api.whatsapp.com/send?phone=447786921525&text=Hi AlbaniaHomes! " target="_blank" className='mt-3 inline-block text-[0.8rem] font-bold text-blue-500'>Send Message</a>
-                </article>
-            </div>
+            
             <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-5 w-[50%] '>
                 <input type="text" name='name' placeholder='Your Full Name' required className='w-full p-6 rounded-md bg-transparent border-2 border-primary-variant text-white resize-none mx-auto' />
                 <input type="email" name='email' placeholder='Your Email' required className='w-full p-6 rounded-md bg-transparent border-2 border-primary-variant text-white resize-none' />
@@ -213,6 +199,21 @@ export default function Home() {
                 <button type='submit' className='bg-white text-blue-950 font-semibold  rounded-md p-3 mx-auto items-center mb-3'>Send Message</button>
             </form>
       </div>
+      <div className="justify-around flex gap-0 p-3 ">
+                <article className='bg-blue-950 w-56 p-5 my-0 rounded-xl text-center border-2 border-blue-950 mx-auto transition hover:bg-transparent hover:border-primary-variant'>
+                    <HiOutlineMail className='text-[1.5rem] mb-2 mx-auto text-white'/>
+                    <h4 className='font-semibold text-white'>Email</h4>
+                    <h5 className='text-white'>info@albaniahomes.co.uk</h5>
+                    <a href="mailto:info@albaniahomes.co.uk" target="_blank" className='mt-3 inline-block text-[0.8rem] text-blue-400 font-semibold'>Send Email</a>
+                </article>
+                
+                <article className='bg-blue-950 w-56 px-9 my-0 py-5 rounded-xl text-center border-2 border-blue-950 transition hover:bg-transparent hover:border-primary-variant mx-auto'>
+                    <ImWhatsapp className='text-[1.5rem] mb-2 mx-auto text-white'/>
+                    <h4 className='text-white font-semibold'>WhatsApp</h4>
+                    <h5 className='text-white'>+447786921525</h5>
+                    <a href="https://api.whatsapp.com/send?phone=447786921525&text=Hi AlbaniaHomes! " target="_blank" className='mt-3 inline-block text-[0.8rem] font-bold text-blue-500'>Send Message</a>
+                </article>
+            </div>
     </div>
     </div>
   );
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -60,17 +60,18 @@ export default function Listing() {
         <div>
           <Swiper navigation>
             {listing.imageUrls.map((url) => (
-              <SwiperSlide key={url}>
-                <div
-                  className='h-[550px]'
-                  style={{
-                    background: `url(${url}) center no-repeat`,
-                    backgroundSize: 'cover',
-                  }}
-                ></div>
-              </SwiperSlide>
-            ))}
+            <SwiperSlide key={url}>
+              <div className="relative w-full h-[550px]">
+                <img
+                src={url}
+                alt="listing"
+                className="w-full h-[660px]"/>
+              </div>
+            </SwiperSlide>))}
           </Swiper>
+
+
+
           <div className='fixed top-[13%] right-[3%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer'>
             <FaShare
               className='text-slate-500'
